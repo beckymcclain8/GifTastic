e1591eff8bd3cf0833c482f6fd65274e653662f8
Becky McClain <beckykelley8@yahoo.com>
2018-03-07 12:42:14 -0700

    loads 10 gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
     "CSI",
     "Survivor"
   ];
-
+  //for loop to create buttons based on the strings in the array
   for (i = 0; i < topics.length; i++) {
     var tv = $("<button>");
     tv.addClass("tv");
@@ -15,4 +15,28 @@ $(document).ready(function() {
     tv.text(topics[i]);
     $("#buttons").append(tv);
   }
+
+  $("button").on("click", function() {
+    var show = $(this).attr("data-name");
+    var queryURL =
+      "https:api.giphy.com/v1/gifs/search?api_key=H7VadhDh2o2nP6aHy83tuseFHFjLSd4C&q=" +
+      show;
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      var results = response.data;
+      for (var i = 0; i < results.length; i++) {
+        var gifDiv = $("<div class='item'>");
+        var rating = results[i].rating;
+        var p = $("<p>").text("Rating: " + rating);
+        var tvImage = $("<img>");
+        tvImage.attr("src", results[i].images.fixed_height.url);
+        gifDiv.append(p);
+        gifDiv.append(tvImage);
+        $("#gifs").prepend(gifDiv);
+      }
+    });
+  });
 });
