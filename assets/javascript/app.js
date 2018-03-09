3599d78e060f8b48ebde48ec59fb291f0b652a19
Becky McClain <beckykelley8@yahoo.com>
2018-03-09 10:26:13 -0700

    adding a button works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,13 +8,17 @@ $(document).ready(function () {
     "Survivor"
   ];
   //for loop to create buttons based on the strings in the array
-  for (i = 0; i < topics.length; i++) {
-    var tv = $("<button>");
-    tv.addClass("tv");
-    tv.attr("data-name", topics[i]);
-    tv.text(topics[i]);
-    $("#buttons").append(tv);
-  }
+
+  function renderButtons() {
+    $("#buttons").empty();
+    for (i = 0; i < topics.length; i++) {
+      var tv = $("<button>");
+      tv.addClass("tv");
+      tv.attr("data-name", topics[i]);
+      tv.text(topics[i]);
+      $("#buttons").append(tv);
+    }
+
   //When the user clicks a button, it sends an AJAX request to the GIPHY API.
   $("button").on("click", function () {
     var show = $(this).attr("data-name");
@@ -33,8 +37,8 @@ $(document).ready(function () {
         var rating = results[i].rating;
         var p = $("<p>").text("Rating: " + rating);
         var tvImage = $("<img>");
-        var still= results[i].images.original_still.url;
-        var animate= results[i].images.original.url
+        var still = results[i].images.original_still.url;
+        var animate = results[i].images.original.url
         tvImage.attr("src", still);
         tvImage.attr("data-still", still);
         tvImage.attr("data-animate", animate);
@@ -44,30 +48,30 @@ $(document).ready(function () {
         gifDiv.prepend(tvImage);
         $("#gifs").prepend(gifDiv);
         console.log(results);
-        };
-      });
-
-      // $(tvImage).on("click", function () {
-      //   $(this).attr("src", $(this).attr(results[i].images.original.url));
-        // $("#gifs").replaceWith(tvImage);
+      };
     });
-    $(".gif").on("click", function() {
-      var state = $(this).attr("data-state");
-    //   // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-    //   // Then, set the image's data-state to animate
-    //   // Else set src to the data-still value
-      if (state === "still") {
-        $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "moving");
-      } else {
-        $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-state", "still");
-      }
-    });
-  
-  
   });
-  
+
+  $(".gif").on("click", function () {
+    var state = $(this).attr("data-state");
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "moving");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  });
+  $("#submit").on("click", function (event) {
+    event.preventDefault();
+    var newTvButton = $("#userInput").val().trim();
+    topics.push(newTvButton);
+    renderButtons();
+  })
+}
+renderButtons();
+});
+
 
 
 
